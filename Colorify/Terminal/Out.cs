3f9073a32ff552bd1e55a34f779f9d015e47a6de
equiman <equiman@users.noreply.github.com>
2017-12-26 12:35:45 -0500

    Fix WriteLine with wrap text

--- Colorify/Terminal/Out.cs
+++ Colorify/Terminal/Out.cs
@@ -9,7 +9,15 @@ namespace Colorify.Terminal
         }
 
         public static void WriteLine(string text){
-            Console.WriteLine( $" {text}" );
+            int size = Console.WindowWidth;
+            if (size != Console.CursorLeft + 1)
+            {
+                size = size - Console.CursorLeft;
+            }
+            if (size < text.Length){
+                size = Console.WindowWidth + size;
+            }
+            Console.Write( $"{text.PadRight(size)}" );
         }
 
         public static void AlignRight(string text)
@@ -19,21 +27,34 @@ namespace Colorify.Terminal
 
         public static void AlignLeft(string text)
         {
-            Console.WriteLine( $"{text.PadRight(Console.WindowWidth)} " );
+            Console.WriteLine( $"{text.PadRight(Console.WindowWidth)}" );
+        }
+
+        public static void AlignCenter(string text){
+            decimal size = Console.WindowWidth - text.Length;
+            int rightSize = (int)Math.Round(size / 2);
+            int leftSize = (int)(size - rightSize);
+            string leftMargin = new String(' ', leftSize);
+            string rightMargin = new String(' ', rightSize);
+
+            Console.Write(leftMargin);
+            Console.Write(text);
+            Console.Write(rightMargin);
         }
         
         public static void Extreme(string left, string right)
         {
-            int r = Console.WindowWidth / 2;
-            int l = Console.WindowWidth - r;
+            decimal size = Console.WindowWidth;
+            int rightMargin = (int)Math.Round(size / 2);
+            int leftMargin = (int)(size - rightMargin);
 
-            Console.Write(left.PadRight(r - 0));
-            Console.WriteLine(right.PadLeft(l - 0));
+            Console.Write($"{left}".PadRight(rightMargin));
+            Console.Write($"{right}".PadLeft(leftMargin));
         }
 
         public static void RepeatLine(char character)
         {
-            string text = new String(character, Console.WindowWidth - 1);
+            string text = new String(character, Console.WindowWidth);
             Console.WriteLine(text);
         }
 
@@ -41,7 +62,7 @@ namespace Colorify.Terminal
         {
             for (int i = 0; i < lines; i++)
             {
-                Console.WriteLine("");
+                RepeatLine(' ');
             }
         }
     }
