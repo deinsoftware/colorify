f2fb1b473da0582145fb509b039fa49848e2635d
equiman <equiman@users.noreply.github.com>
2018-01-12 15:55:22 -0500

    Secure SonarCloud token

--- sonar.bat
+++ sonar.bat
@@ -1,8 +1,8 @@
 @echo off
-
+set /p token=<sonar.txt
 :: https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-SonarQube.Scanner.MSBuild.exe begin /k:"dein:colorify" /n:"Colorify" /v:"1.0.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="34dadb57c7435b016b8ba12e48dc49f5d1eadee6" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+SonarQube.Scanner.MSBuild.exe begin /k:"dein:colorify" /n:"Colorify" /v:"1.0.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test Colorify.Tests/Colorify.Tests.csproj
-SonarQube.Scanner.MSBuild.exe end /d:sonar.login="34dadb57c7435b016b8ba12e48dc49f5d1eadee6"
+SonarQube.Scanner.MSBuild.exe end /d:sonar.login="%token%"
