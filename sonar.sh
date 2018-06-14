25d326afa0327aed79ddbeeae16cc20770bf8b9e
equiman <equiman@users.noreply.github.com>
2018-06-14 10:29:56 -0500

    Sonar scanner update for mac

--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 token="$(cat sonar.txt)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe begin /k:"dein:colorify" /n:"Colorify" /v:"1.0.6" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll begin /k:"dein:colorify" /n:"Colorify" /v:"1.0.6" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test Colorify.Tests/Colorify.Tests.csproj
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe end /d:sonar.login="${token}"
+dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll end /d:sonar.login="${token}"
