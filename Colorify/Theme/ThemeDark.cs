d581ee660901357cc6fe8abe39a1d02f78d17b1a
equiman <equiman@users.noreply.github.com>
2020-04-24 13:34:20 -0500

    Reorder Theme text and backgrounds

--- Colorify/Theme/ThemeDark.cs
+++ Colorify/Theme/ThemeDark.cs
@@ -40,17 +40,17 @@ namespace Colorify.UI
         {
             var colors = new Dictionary<string, Color>();
             colors.Add("text-default", AddColor(null, null));
-            colors.Add("bg-default", AddColor(null, null));
             colors.Add("text-muted", AddColor(null, ConsoleColor.DarkGray));
             colors.Add("text-primary", AddColor(null, ConsoleColor.Gray));
             colors.Add("text-warning", AddColor(null, ConsoleColor.Yellow));
             colors.Add("text-danger", AddColor(null, ConsoleColor.Red));
+            colors.Add("text-success", AddColor(null, ConsoleColor.DarkGreen));
+            colors.Add("text-info", AddColor(null, ConsoleColor.DarkCyan));
+            colors.Add("bg-default", AddColor(null, null));
             colors.Add("bg-muted", AddColor(ConsoleColor.DarkGray, ConsoleColor.Black));
             colors.Add("bg-primary", AddColor(ConsoleColor.Gray, ConsoleColor.White));
             colors.Add("bg-warning", AddColor(ConsoleColor.Yellow, ConsoleColor.Black));
             colors.Add("bg-danger", AddColor(ConsoleColor.Red, ConsoleColor.White));
-            colors.Add("text-success", AddColor(null, ConsoleColor.DarkGreen));
-            colors.Add("text-info", AddColor(null, ConsoleColor.DarkCyan));
             colors.Add("bg-success", AddColor(ConsoleColor.DarkGreen, ConsoleColor.White));
             colors.Add("bg-info", AddColor(ConsoleColor.DarkCyan, ConsoleColor.White));
             base._colors = colors;
--- Colorify/Theme/ThemeLight.cs
+++ Colorify/Theme/ThemeLight.cs
@@ -40,17 +40,17 @@ namespace Colorify.UI
         {
             var colors = new Dictionary<string, Color>();
             colors.Add("text-default", AddColor(null, null));
-            colors.Add("bg-default", AddColor(null, null));
             colors.Add("text-muted", AddColor(null, ConsoleColor.Gray));
             colors.Add("text-primary", AddColor(null, ConsoleColor.DarkGray));
             colors.Add("text-warning", AddColor(null, ConsoleColor.DarkYellow));
             colors.Add("text-danger", AddColor(null, ConsoleColor.DarkRed));
+            colors.Add("text-success", AddColor(null, ConsoleColor.DarkGreen));
+            colors.Add("text-info", AddColor(null, ConsoleColor.DarkCyan));
+            colors.Add("bg-default", AddColor(null, null));
             colors.Add("bg-muted", AddColor(ConsoleColor.Gray, ConsoleColor.Black));
             colors.Add("bg-primary", AddColor(ConsoleColor.DarkGray, ConsoleColor.White));
             colors.Add("bg-warning", AddColor(ConsoleColor.DarkYellow, ConsoleColor.White));
             colors.Add("bg-danger", AddColor(ConsoleColor.DarkRed, ConsoleColor.White));
-            colors.Add("text-success", AddColor(null, ConsoleColor.DarkGreen));
-            colors.Add("text-info", AddColor(null, ConsoleColor.DarkCyan));
             colors.Add("bg-success", AddColor(ConsoleColor.DarkGreen, ConsoleColor.White));
             colors.Add("bg-info", AddColor(ConsoleColor.DarkCyan, ConsoleColor.White));
             base._colors = colors;
